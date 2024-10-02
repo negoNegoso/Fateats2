e5f7a8a0acb2e11d8839c4440b7fccc28f0eb006
yvillan <114770837+yvillan@users.noreply.github.com>
2024-10-02 16:34:42 -0300

    feat:HomeScreen Component

--- app/src/main/java/com/fatec/fateats2/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/fatec/fateats2/ui/screens/HomeScreen.kt
@@ -1,23 +1,36 @@
 package com.fatec.fateats2.ui.screens
 
-import android.media.midi.MidiDeviceInfo.PortInfo
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fatec.fateats2.model.Product
 import com.fatec.fateats2.sampledata.sampleCandies
 import com.fatec.fateats2.sampledata.sampleDrinks
 import com.fatec.fateats2.sampledata.sampleProducts
+import com.fatec.fateats2.sampledata.sampleSections
+import com.fatec.fateats2.ui.components.CardProductItem
 import com.fatec.fateats2.ui.components.CustomTopAppBar
 import com.fatec.fateats2.ui.components.ProductsSection
 import com.fatec.fateats2.ui.components.SearchTextField
+import com.fatec.fateats2.ui.theme.Fateats2Theme
 
 class HomeScreenUiState(
-    val sectios: Map<String, List<Product>> = emptyMap(),
+    val sections: Map<String, List<Product>> = emptyMap(),
     val searchedProducts: List<Product> = emptyList(),
     val searchText: String = "",
     val onSearchChange: (String) -> Unit = {}
@@ -29,7 +42,7 @@ class HomeScreenUiState(
 
 @Composable
 fun HomeScreen(products: List<Product> = sampleProducts) {
-    val sectios = mapOf(
+    val sections = mapOf(
         "Todos Produtos" to products,
         "Promoção" to sampleDrinks + sampleCandies,
         "Doces" to sampleCandies,
@@ -37,21 +50,104 @@ fun HomeScreen(products: List<Product> = sampleProducts) {
     )
     var text by remember { mutableStateOf("") }
 
-    Column()
-    {
+    fun containsInNameOrDescrioption() = { product: Product ->
+        product.name.contains(
+            text,
+            ignoreCase = true,
+        ) || product.description?.contains(
+            text,
+            ignoreCase = true,
+        ) ?: false
+    }
+
+    val searchedProducts = remember(text, products) {
+        if (text.isNotBlank()) {
+            (sampleProducts + products).filter(containsInNameOrDescrioption())
+        } else emptyList()
+    }
+
+    val state = remember(products, text) {
+        HomeScreenUiState(
+            sections = sections,
+            searchedProducts = searchedProducts,
+            searchText = text,
+            onSearchChange = {
+                text = it
+            }
+        )
+    }
+    HomeScreen(state = state)
+}
+
+@Composable
+fun HomeScreen(
+    state: HomeScreenUiState = HomeScreenUiState()
+) {
+    Column {
+        val sections = state.sections
+        val text = state.searchText
+        val searchedProducts = state.searchedProducts
+
         CustomTopAppBar(title = "Home")
 
         SearchTextField(
             searchText = text,
-            onSearchChange = { newText ->
-                text = newText
-                Log.i("HomeScreen", "HomeScreen: TextField: $newText")
-                Log.i("HomeScreen", "HomeScreen: state: $text")
-            })
-
-        ProductsSection(
-            "Promoções",
-            products = products
+            onSearchChange = state.onSearchChange,
+            Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
         )
+
+        LazyColumn(
+            Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(bottom = 16.dp)
+        ) {
+            if (state.isShowSections()) {
+                for (section in sections) {
+                    val title = section.key
+                    val products = section.value
+                    item {
+                        ProductsSection(
+                            title = title,
+                            products = products
+                        )
+                    }
+                }
+            } else {
+                items(searchedProducts) { p ->
+                    CardProductItem(
+                        product = p,
+                        Modifier.padding(horizontal = 16.dp),
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun HomeScreenPreview() {
+    Fateats2Theme {
+        Surface {
+            HomeScreen(HomeScreenUiState(sections = sampleSections))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenWithSearchTextPreview() {
+    Fateats2Theme {
+        Surface {
+            HomeScreen(
+                state = HomeScreenUiState(
+                    searchText = "a",
+                    sections = sampleSections
+                ),
+            )
+        }
     }
 }
